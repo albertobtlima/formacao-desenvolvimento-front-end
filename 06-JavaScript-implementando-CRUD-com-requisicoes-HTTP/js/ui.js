c6ab0849f2f0cfbb9ab0fbab759c1dd8494b4b24
Alberto Lima <alberto.btlima@outlook.com>
2024-12-24 15:23:10 -0300

    add: api.js

---
+++ 06-JavaScript-implementando-CRUD-com-requisicoes-HTTP/js/ui.js
@@ -0,0 +1,10 @@
+import api from "./api.js";
+
+const ui = {
+  async renderizarPensamentos() {
+    const listaPensamentos = document.getElementById("lista-pensamentos");
+
+    try {
+    } catch {}
+  },
+};
